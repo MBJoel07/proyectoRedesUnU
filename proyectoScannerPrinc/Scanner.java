6ec0989d3de265727984aba6ecac6a580e7f300f
yoxdddd <beltran.mauroet36@gmail.com>
2025-08-05 18:45:42 -0300

    Agregar la carpeta proyectoScannerPrinc

---
+++ proyectoScannerPrinc/Scanner.java
@@ -0,0 +1,33 @@
+package proyectoRedesJava;
+
+public class Scanner {
+    private int timeout;
+
+    public Scanner(int timeout) {
+        this.timeout = timeout;
+    }
+
+    public boolean ipValida(String ip) {
+    	String[] partes = ip.split("\\.");
+
+        // 2. Verificamos que tenga exactamente 4 partes
+        if (partes.length != 4) return false;
+
+        // 3. Validamos cada parte
+        for (String parte : partes) {
+            try {
+                int numero = Integer.parseInt(parte);
+                if (numero < 0 || numero > 255) return false;
+            } catch (NumberFormatException e) {
+                // Si no se puede convertir a número, no es válido
+                return false;
+            }
+        }
+
+        // 4. Si todo está bien, es válida
+        return true;
+    }
+
+    
+}
+
